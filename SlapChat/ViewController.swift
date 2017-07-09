2bd895f1a6f89e166b13ddc9728efdf6be3a93a6
Mark Rabins <markrabins@gmail.com>
2017-07-09 00:00:05 -0700

    Import Assets, Some of Apple's Code and Minor refactoring

--- SlapChat/ViewController.swift
+++ SlapChat/ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: AAPLCameraViewController {
 
+    @IBOutlet weak var previewView: AAPLPreviewView!
+    
     override func viewDidLoad() {
+        self._previewView = previewView
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    
 
 }
 
