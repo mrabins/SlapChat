c2d0f9940c73627762e57eab89af577b89f85920
Mark Rabins <markrabins@gmail.com>
2017-07-11 16:44:41 -0700

    Firebase Pod Set Up, UI for log in view

---
+++ SlapChat/RoundTextField.swift
@@ -0,0 +1,46 @@
+//
+//  RoundTextField.swift
+//  SlapChat
+//
+//  Created by Mark Rabins on 7/11/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundTextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let placeholderString = NSAttributedString(string: rawString, attributes: [NSForegroundColorAttributeName: placeholderColor!])
+            attributedPlaceholder = placeholderString
+        }
+    }
+}
---
+++ SlapChat/RoundedButton.swift
@@ -0,0 +1,41 @@
+//
+//  RoundedButton.swift
+//  SlapChat
+//
+//  Created by Mark Rabins on 7/11/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+
+}
